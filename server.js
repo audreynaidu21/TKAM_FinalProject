b322515a838448a0fb384b9a786b1a6c3ec65b5c
Audrey <audrey.naidu.21@bishops.com>
2017-02-12 16:34:17 -0800

    test1

---
+++ server.js
@@ -0,0 +1,11 @@
+var PORT = 3000;
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+var port = process.env.PORT || PORT
+app.listen(port, function(){
+
+	console.log("Listening on %s", port);
+
+});
